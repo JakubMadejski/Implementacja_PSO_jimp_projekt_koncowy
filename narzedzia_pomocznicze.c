313ef75a551a49a7a4aef25f462d942956158adb
JakubMadejski <01206793@pw.edu.pl>
2025-12-24 14:23:55 +0100

    Narzędzia pomocznicznie

---
+++ narzedzia_pomocznicze.c
@@ -0,0 +1,10 @@
+#include "narzedzia_pomocznicze.h"
+#include <time.h>
+
+double random_double() {
+    return (double)rand() / (double)RAND_MAX;
+}
+
+double random_range(double min, double max) {
+    return min + random_double() * (max - min);
+}
---
+++ narzedzia_pomocznicze.h
@@ -0,0 +1,10 @@
+#ifndef NARZEDZIA_POMOCZNICZE_H
+#define NARZEDZIA_POMOCZNICZE_H
+
+#include <stdlib.h>
+
+double random_double();
+
+double random_range(double min, double max);
+
+#endif
