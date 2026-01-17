ca9e44e4f18865f59f8eb56effdbaa52dde341f5
JakubMadejski <01206793@pw.edu.pl>
2026-01-17 16:25:06 +0100

    zmiana kompilacji

--- main.c
+++ main.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "pso.h"
 #include "obsluga_map.h"
 #include "logger.h"
 
+
+static void wczytaj_config(const char* nazwa, double* waga, double* c1, double* c2) {
+    if (nazwa == NULL) return;
+    FILE* f = fopen(nazwa, "r");
+    if (f == NULL) return;
+    if (fscanf(f, "%lf %lf %lf", waga, c1, c2) != 3) {
+    }
+    fclose(f);
+}
+
 int main(int argc, char** argv) {
-    const char* mapa_file = (argc > 1) ? argv[1] : "test_map.txt";
+    if (argc < 2) {
+        fprintf(stderr, "Uzycie: %s <plik_mapy> -p <liczba_czasteczek> -i <liczba_iteracji> -c <plik_konfig> -n <co_n>\n", argv[0]);
+        return 1;
+    }
+
+    const char* mapa_file = argv[1];
+    int liczba_czasteczek = 30;
+    int iteracje = 100;
+    const char* config_file = NULL;
+    int zapisz_co_n = 1;
     const char* log_file = "log.csv";
+
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
+            liczba_czasteczek = atoi(argv[++i]);
+        } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+            iteracje = atoi(argv[++i]);
+        } else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
+            config_file = argv[++i];
+        } else if (strcmp(argv[i], "-n") == 0 && i + 1 < argc) {
+            zapisz_co_n = atoi(argv[++i]);
+            if (zapisz_co_n <= 0) zapisz_co_n = 1;
+        } else {
+            fprintf(stderr, "Nieznana flaga lub brak argumentu: %s\n", argv[i]);
+            return 1;
+        }
+    }
+
+    double waga = 0.5, c1 = 1.0, c2 = 1.0;
+    wczytaj_config(config_file, &waga, &c1, &c2);
 
     srand((unsigned)time(NULL));
 
@@ -20,17 +59,16 @@ int main(int argc, char** argv) {
     inicjalizuj_logger(log_file);
 
     Roj roj;
-    int liczba_czastek = 30;
-    int iteracje = 100;
-    double waga = 0.5, c1 = 1.0, c2 = 1.0;
 
-    inicjalizuj_roj(&roj, liczba_czastek, mapa->szerokosc, mapa->wysokosc, mapa);
+    inicjalizuj_roj(&roj, liczba_czasteczek, mapa->szerokosc, mapa->wysokosc, mapa);
 
     zapisz_stan_roju(log_file, 0, &roj);
 
     for (int it = 1; it <= iteracje; it++) {
         aktualizuj_roj(&roj, waga, c1, c2, mapa);
-        zapisz_stan_roju(log_file, it, &roj);
+        if (zapisz_co_n > 0 && (it % zapisz_co_n) == 0) {
+            zapisz_stan_roju(log_file, it, &roj);
+        }
     }
 
     free(roj.czasteczki);
